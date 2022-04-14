046fc409540dfbe4d7868152318923672a1b27ee
ilja.tihhonov <ilja.tihhonov@outlook.com>
2022-04-14 14:51:18 +0300

    limited tries

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GuessingGameThreeTries
+{ 
+    class Program
+    
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int myRandom = rnd.Next(1, 11);
+            int counter = 3;
+            Console.WriteLine("Try to guess my number! It is from 0 to 10 only! But remember, ytou have three tries only!");
+            
+
+            while (counter > 0)
+            {
+
+                int answer = Convert.ToInt32(Console.ReadLine());
+                if (answer == myRandom)
+                {
+                    --counter;
+                    Console.WriteLine($"You are right, the answer is {answer}!");
+                    break;
+                }
+                else
+                {
+                    --counter;
+                    Console.WriteLine($"WRONG! {counter} tries left!");
+                }
+            }
+
+
+        }
+
+    }
+}
